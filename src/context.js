b5613fad35c3c58c2c3915d69f5a2264e7f8788f
Jeremy Hawkins <jeremy.hawkins1@hccs.edu>
2019-01-05 15:44:02 -0600

    Refactored Context Component will Mount

--- src/context.js
+++ src/context.js
@@ -26,62 +26,60 @@ export default class Provider extends Component {
   componentDidMount() {
     const api = "https://online-03.hccs.edu/toolkit/api"
     const siloStore = []
-    console.log("Getting Silos...")
+    
+    console.log("Getting Data")
     axios.get(`${api}/silos`)
     .then(response => {
-      if (response.status === 200) {
-        console.log("Recieved Silos")
-        const siloData = response.data.data
-        siloData.forEach(silo => {
+      if (response.status === 200){
+        let results = response.data
+        results.data.forEach(silo => {
           siloStore[silo._id] = {
             id: silo._id,
             title: silo.title,
             groups: []
           }
-          console.log("Getting Groups for Silo "+silo._id)
+          console.log("Gettin Groups for Silo "+silo._id)
           axios.get(`${api}/silos/${silo._id}/groups`)
-           .then(response => {
-             if (response.status === 200) {
-               console.log("Recieved Groups for Silo "+silo._id)
-               const groupData = response.data.data
-               groupData.forEach(group => {
-                 siloStore[silo._id].groups[group._id] = {
-                   id: group._id,
-                   title: group.title,
-                   commands: []
-                 }
-                 console.log(siloStore)
-                 console.log("getting Commands for Group "+group._id)
-                 axios.get(`${api}/silos/${silo._id}/groups/${group._id}/commands`)
-                 .then(response => {
-                   if (response.status === 200){
-                     console.log("Recieved Commands for Group "+group._id)
-                     const commandData = response.data.data
-                     commandData.forEach(command => {
-                       siloStore[silo._id]['groups'][group._id]['commands'][command._id] = {
-                         id: command._id,
-                         title: command.title,
-                         details: command.details
-                       }
-                     })
-                   } else {
-                     console.log("No Commands for Group "+group._id)
-                   }
-                   console.log("Updating State to: ",siloStore)
-                   this.setState({
-                      silos: siloStore
-                   })
-                 })
-               })
-             } else {
-               console.log("No Groups for Silo "+silo._id)
-             }
-           })
-        })
+          .then(response => {
+            if (response.status === 200) {
+              console.log("Recieved Groups for Silo "+silo._id)
+              let results = response.data
+              results.data.forEach(group => {
+                siloStore[silo._id]['groups'][group._id] = {
+                  id: group._id,
+                  title: group.title,
+                  commands: []
+                }
+                console.log("Getting Commands for Group "+group._id)
+                axios.get(`${api}/silos/${silo._id}/groups/${group._id}/commands`)
+                .then(response => {
+                  if (response.status === 200){
+                    let results = response.data
+                    results.data.forEach(command => {
+                      siloStore[silo._id]['groups'][group._id]['commands'][command._id] = {
+                        id: command._id,
+                        title: command.title,
+                        details: command.details
+                      }
+                    })
+                  } else {
+                    console.log("No Commands for Group "+group._id)
+                  }
+                  console.log("All Data Collection Should be Complete...")
+                  console.log("Adding Silos to State", siloStore)
+                  this.setState({ silos: siloStore })
+                })
+              })
+            } else {
+              console.log("No Groups for Silo "+silo._id)
+            }
+          })
+        }) 
       } else {
-        console.log("No Silos Recieved")
+        console.log("No Data Recieved")
       }
     })
+
   }
   render() {
     return (
